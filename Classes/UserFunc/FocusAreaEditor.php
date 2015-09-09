f07f601db0f311431013e1fdfbdfaabaa5bed2b2
Sven Radetzky <sr@chiari.de>
2015-09-09 15:13:15 +0200

    [Bugfix] Backend editor maximum image size

--- Classes/UserFunc/FocusAreaEditor.php
+++ Classes/UserFunc/FocusAreaEditor.php
@@ -118,6 +118,8 @@ class FocusAreaEditor {
 
                         // Start Jcrop plugin
                         $("#focusareaeditor > img").Jcrop({
+                            boxWidth: 800,
+                            boxHeight: 600,
                             onSelect: changeFields,
                             onChange: changeFields,
                             trueSize: [width,height],
